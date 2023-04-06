24a0d0c2f5fb57fef13bf778728c0a0f8dbbf581
ap1k86 <ap1k86@yandex.ru>
2023-04-06 21:05:13 +0300

    Изменил размер картинок на 500х400. Сделал 2 разные формы. Добавил иконку юзера. Добавил пагинацию с 2-х сторон.

--- blog_pr/myblog/forms.py
+++ blog_pr/myblog/forms.py
@@ -44,10 +44,12 @@ class SignInForm(forms.Form):
     username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={
         'class': "form-control",
         'id': "inputUsername",
+        'placeholder': 'Логин'
     }))
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
         'class': "form-control mt-2",
         'id': "inputPassword",
+        'placeholder': 'Пароль'
     }))
 
 
